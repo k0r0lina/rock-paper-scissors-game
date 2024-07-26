03746bbe1c0f14842f8664220cf59ad6b0e10334
k0r0lina <karolina.rzasa2002@gmail.com>
2024-07-26 12:42:02 +0200

    Add html for body && Add messages functions for index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("Working");
+let computerMove = "rock";
+let playerMove = "paper";
+printMessage(
+  "I played " + computerMove + "! If you played " + playerMove + ", you win!"
+);
+
+// Messages functions
+function printMessage(msg) {
+  var div = document.createElement("div");
+  div.innerHTML = msg;
+  document.getElementById("messages").appendChild(div);
+}
+
+function clearMessages() {
+  document.getElementById("messages").innerHTML = "";
+}
